147e6b2bf8cc9a7f37d701d4b3fa13d025340b49
BigMax <bigmax@bigmax-VirtualBox.(none)>
2013-03-26 16:55:02 +0200

    Added admin page

--- mysite/mysite/apps/results/models.py
+++ mysite/mysite/apps/results/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Result(models.Model):
+
+	first_name = models.CharField(max_length=30)
+	last_name = models.CharField(max_length=30)
+	
+
+
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -14,9 +14,9 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': rel('mysite.db'),               # Or path to database file if using sqlite3.
+            # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
@@ -125,10 +125,13 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    
+    'mysite.apps.results',
 )
 
 # A sample logging configuration. The only tangible logging
